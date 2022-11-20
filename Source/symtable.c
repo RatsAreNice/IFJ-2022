56cd52971e3e81501eb3d5a588f67ab54a3c0827
Rat <ratfishman326@gmail.com>
2022-11-20 23:11:58 +0100

    commit pred zapocatim zebrackeho debuggingu

--- Source/symtable.c
+++ Source/symtable.c
@@ -13,12 +13,12 @@ JADRO PREBRANE Z VYPRACOVANIA 2. DOMACEJ ULOHY PREDMETU IAL MATUSA DOBIASA
 
 // Uzol stromu
 typedef struct bst_node {
-  char* key;               // kľúč
-  int type;              // typ
-  funData_t* funData;
-  labelData_t* labelData;
-  struct bst_node *left;  // ľavý potomok
-  struct bst_node *right; // pravý potomok
+  char* key;               
+  int type;              
+  funData_t* funData; // data funkcie
+  //labelData_t* labelData;
+  struct bst_node *left;  
+  struct bst_node *right; 
 } bst_node_t;
 
 // data
@@ -34,10 +34,11 @@ typedef struct funData
 
 
 
-typedef struct labelData
+/*typedef struct labelData
 {
   bool declared;
-} labelData_t;
+} labelData_t;*/
+
 
 
 
@@ -80,6 +81,179 @@ bst_node_t* bst_search(bst_node_t *tree, char* key) {
     }
   }
   
-  return false;
+  return NULL;
 }
+
+// RETRUNS 1 -> SUCCESSFUL INSERT
+// RETURNS 2 -> SUCCESFUL REPLACE
+// RETURNS 99 -> MALLOC SEX
+int bst_insert(bst_node_t **tree, char* key, int type, funData_t* funData) {
+  int diff = 0;
+  if (*tree!=NULL)
+  {
+    diff = strcmp(tree->key,key);
+  }
+  // naplast solution, nechcelo sa mi refaktorizovat
+  if (*tree==NULL)
+  {
+    bst_node_t* temp = (bst_node_t*)malloc(sizeof(struct bst_node));
+    temp->type=type;
+    temp->funData=funData;
+    temp->key=key;
+    temp->left=NULL;
+    temp->right=NULL;
+    
+    if (temp==NULL)
+    {
+      return 99; //MALLOC FAIL
+    }
+    *tree=temp;
+    return 1;
+  }
+  else if (diff>0)
+  {
+    return bst_insert(&(*tree)->left,key,type,&funData);
+  }
+  else if (diff<0)
+  {
+    return bst_insert(&(*tree)->right,key,type,&funData);
+  }
+  else
+  {
+    (*tree)->value=value;
+    return 2;
+  }
+
+}
+
+void bst_replace_by_rightmost(bst_node_t *target, bst_node_t **tree) {
+  if ((*tree)->right->right!=NULL)
+  {
+    bst_replace_by_rightmost(target,&(*tree)->right);
+  }else
+  //(*tree)->right je ptr
+  //(*tree) je previous
+  {
+    target->key=(*tree)->right->key;  //target kluc nahradeny rightmost klucom
+    target->type=(*tree)->right->type; //target value nahradeny rightmost value
+    funData_t* temp2 = target->funData;
+    target->funData=(*tree)->right->funData;
+    bst_node_t* temp=(*tree)->right;
+    (*tree)->right=(*tree)->right->left; // otec rightmost uzlu zdedil lavu hranu svojho syna
+    free(temp2);
+    free(temp); 
+  }
+}
+
+
+void bst_delete(bst_node_t **tree, char* key) {
+  int diff = 0
+  if ((*tree)!=NULL)
+  {
+    diff = strcmp((*tree)->key,key);
+  }
+  
+  if ((*tree)==NULL)
+  {
+    
+    return;
+  } // prazdny koren
+  if (diff==0) //mazany kluc je v koreni alebo ma 2 synov
+  {
+    
+    bst_replace_by_rightmost(*tree,&(*tree)->left);
+    return;
+  }
+  if (((*tree)->left == NULL && diff<0 )||((*tree)->right == NULL && diff>0 ))
+  {
+    //node doesnt exist
+    return;
+  }
+  
+  if ((*tree)->left!=NULL) // zistujem ci v lavo nieje hladany uzol
+  {
+    int diff2 = strcmp((*tree)->left->key,key);
+    if (diff2 == 0)
+    {
+      
+      if ((*tree)->left->left!=NULL && (*tree)->left->right!=NULL)
+      {
+        bst_delete(&(*tree)->left,key);
+        return;
+      }else
+      {
+        // mazany node ma 0 alebo jeden poduzol
+        bst_node_t* temp=(*tree)->left;
+        if ((*tree)->left->left!=NULL)
+        {
+          (*tree)->left=temp->left;
+        }else
+        {
+          (*tree)->left=temp->right;
+        }
+        free(temp->funData);
+        free(temp); 
+        return;
+      }
+    }
+    
+  }
+  if ((*tree)->right!=NULL)
+  {
+    int diff2 = strcmp((*tree)->left->key,key);
+    if (diff2 == 0)
+    {
+      
+      if ((*tree)->right->left!=NULL && (*tree)->right->right!=NULL)
+      {
+        bst_delete(&(*tree)->right,key);
+        return;
+      }else
+      {
+        // mazany node ma 0 alebo jeden poduzol
+        bst_node_t* temp=(*tree)->right;
+        if ((*tree)->right->left!=NULL)
+        {
+          (*tree)->right=temp->left;
+        }else
+        {
+          (*tree)->right=temp->right;
+        }
+        free(temp->funData);
+        free(temp); 
+        return;
+      }
+    }
+  }
+
+  
+  if (diff<0)
+  {
+    bst_delete(&(*tree)->left,key);
+    return;
+  } else
+  {
+    bst_delete(&(*tree)->right,key);
+    return;
+  }
+
+  return;
+}
+
+void bst_dispose(bst_node_t **tree) {
+  
+  if ((*tree)!=NULL)
+  {
+    bst_dispose(&(*tree)->left);
+    bst_dispose(&(*tree)->right);
+    (*tree)->left=NULL;
+    (*tree)->right=NULL;
+    free((*tree)->funData);
+    free(*tree); // 116 -> 220
+  }
+  *tree=NULL;
+
+}
+
+
 
