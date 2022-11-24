362d61a5c32a36a0230f85d3e2b48288373a9d9b
Rat <ratfishman326@gmail.com>
2022-11-24 20:20:58 +0100

    farmim commity

--- Source/bottom_up.h
+++ Source/bottom_up.h
@@ -1,6 +1,7 @@
 #ifndef BOTTOM_UP_H
 #define BOTTOM_UP_H
 #include "scanner.h"
+#include "ASS.h"
 int convert(token_t a);
 int get_input(token_type ** first, token_type ** second, token_t ** token, token_type end, token_type end2,int skip,int* bracketcount);
 int cmp_to_rule(int rs[]);
